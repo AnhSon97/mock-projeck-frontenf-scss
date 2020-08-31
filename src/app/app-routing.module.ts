afbbbcc53be9107053f7695df02420b53fd5d5d1
AnhSon97 <nguyenanhson.997@gmail.com>
2020-08-31 21:50:12 +0700

    add component login + footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from '../app/login/login.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [{path: "", component: LoginComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MainSidebarContainerComponent } from './main-sidebar-container/main-sidebar-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    FooterComponent,
+    NavBarComponent,
+    MainSidebarContainerComponent
   ],
   imports: [
     BrowserModule,

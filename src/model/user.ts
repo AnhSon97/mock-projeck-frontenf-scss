d5046d4de149544f4f49796255401d65ed234c97
AnhSon97 <nguyenanhson.997@gmail.com>
2020-09-01 17:19:19 +0700

    add setting account

---
+++ src/model/user.ts
@@ -0,0 +1,20 @@
+export class User {
+userID: string;
+source: string;
+firstname: string;
+lastname: string;
+username: string;
+gender: string;
+phonenumber: string
+email: string;
+password: string;
+bithdate: Date;
+school: string;
+address: string;
+socialNetwork: string;
+status: string;
+historyTransaction: string;
+createdate: Date;
+enable: number;
+token: string;
+}

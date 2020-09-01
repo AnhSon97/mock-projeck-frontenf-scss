f3a54d8d8e22349e5d1e23021ba11f1b5fbb8bfd
AnhSon97 <nguyenanhson.997@gmail.com>
2020-09-01 10:41:09 +0700

    create component login,logged

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from '../app/login/login.component'
+import { MainSidebarContainerComponent } from './main-sidebar-container/main-sidebar-container.component';
 
 
-const routes: Routes = [{path: "", component: LoginComponent}];
+const routes: Routes = [
+  { path: "", component: LoginComponent },
+  { path: "logged", component: MainSidebarContainerComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
